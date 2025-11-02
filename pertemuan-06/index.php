5ad23142d7b4390f1253c9651568a8a9608cc4e4
25111500007-Jeffre6098 <2511500007@mahasiswa.atmaluhur.ac.id>
2025-11-02 14:05:22 +0700

    Redesign font family and  fontstyle

--- pertemuan-06/index.php
+++ pertemuan-06/index.php
@@ -5,7 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>"> <!-- Supaya pas di refresh halaman style.cssnya gak ke cache -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Anton&family=Bungee&family=Oswald:wght@200..700&display=swap" rel="stylesheet">    
+<link rel="stylesheet" href="style.css?v=<?php echo time(); ?>"> <!-- Supaya pas di refresh halaman style.cssnya gak ke cache -->
 </head>
 <script>
     window.onbeforeunload = function() { // Biar pas refresh halaman, posisi scrollnya balik ke atas
@@ -25,7 +28,7 @@
                 <li><a href="#about">About</a></li>
                 <li><a href="#gpa">Gpa</a></li>
                 <li><a href="#contact">Contact</a></li>
-                
+
             </ul>
         </nav>
     </header>
@@ -100,16 +103,16 @@
             $nilaiUTS5 = 90;
             $nilaiUAS5 = 86;
             $nilaiAkhir5 = (0.1 * $nilaiHadir5) + (0.2 * $nilaiTugas5) + (0.3 * $nilaiUTS5) + (0.4 * $nilaiUAS5);
-// Diatas ini adalah variabel untuk store value dari tiap variabel dan menghitung nilai akhir
+            // Diatas ini adalah variabel untuk store value dari tiap variabel dan menghitung nilai akhir
 
             function hitungGrade($hadir, $akhir): string // function ini untuk ngasih tau mesin untuk store value ke variabel sementara
             {
                 if ($hadir < 70) return "E"; // if = jika kehadiran dibawah 70 langsung E
                 elseif ($akhir >= 90) return "A"; // ifelse = jika tidak nilai akhir diatas sama dengan 90 return A
-                elseif ($akhir >= 80) return "A-";// tapi cek lagi kalo nilai akhir dibawah 90 tapi diatas sama dengan 80 return A-
-                elseif ($akhir >= 75) return "B+";// jadi atau kembali = return fungsinya untuk memberikan hasil dari function ke variabel menjadi value jika ifelse mendeteksi valuenya sesuai
-                elseif ($akhir >= 70) return "B";// tapi kalo di cek sama ifelse nya ga sesuai, si kode returnnya lanjut ngecek kebawah terus sampai perintah ifelse nya true
-                elseif ($akhir >= 65) return "B-";// kalo ga sesuai semua, jadi else return E
+                elseif ($akhir >= 80) return "A-"; // tapi cek lagi kalo nilai akhir dibawah 90 tapi diatas sama dengan 80 return A-
+                elseif ($akhir >= 75) return "B+"; // jadi atau kembali = return fungsinya untuk memberikan hasil dari function ke variabel menjadi value jika ifelse mendeteksi valuenya sesuai
+                elseif ($akhir >= 70) return "B"; // tapi kalo di cek sama ifelse nya ga sesuai, si kode returnnya lanjut ngecek kebawah terus sampai perintah ifelse nya true
+                elseif ($akhir >= 65) return "B-"; // kalo ga sesuai semua, jadi else return E
                 elseif ($akhir >= 60) return "C+";
                 elseif ($akhir >= 55) return "C";
                 elseif ($akhir >= 50) return "C-";
@@ -118,12 +121,12 @@
             }
 
             function hitungMutu($grade): float // function ini ngehitung $grade1 sampai $grade5, cuma di store ke variabel sementara biar ga harus nulis semua perhitungan grade          
-             {
+            {
                 switch ($grade) { // switch dan case fungsinya sama kaya ifelse, cuma ini lebih rapi kalo banyak kondisi yang mau dicek
                     case "A":
-                        return 4.0;// float itu tipe data desimal
+                        return 4.0; // float itu tipe data desimal
                     case "A-":
-                        return 3.7;// sebenarnya bisa juga ditulis if ($grade == "A-") { return 3.7; } tapi kalo banyak kondisi mending pake switch case biar rapi dan ga panjang :v
+                        return 3.7; // sebenarnya bisa juga ditulis if ($grade == "A-") { return 3.7; } tapi kalo banyak kondisi mending pake switch case biar rapi dan ga panjang :v
                     case "B+":
                         return 3.3;
                     case "B":
@@ -148,8 +151,8 @@
             $grade3 = hitungGrade($nilaiHadir3, $nilaiAkhir3);
             $grade4 = hitungGrade($nilaiHadir4, $nilaiAkhir4);
             $grade5 = hitungGrade(hadir: $nilaiHadir5, akhir: $nilaiAkhir5); // nah ini dia variabel kayak $nilaiHadir1-5 disetore dalam satu variabel $hadir di dalam variabel $grade1-5
-// biar ngehitung dan nulis kode untuk ngehitungnya grade1-5 ga manual satu per satu. capek nulisnya :,)
-// kalo ga salah bisa disebut parameter named arguments
+            // biar ngehitung dan nulis kode untuk ngehitungnya grade1-5 ga manual satu per satu. capek nulisnya :,)
+            // kalo ga salah bisa disebut parameter named arguments
             $mutu1 = hitungMutu($grade1);
             $mutu2 = hitungMutu($grade2);
             $mutu3 = hitungMutu($grade3);
@@ -173,8 +176,8 @@
             $totalSKS = $sksMatkul1 + $sksMatkul2 + $sksMatkul3 + $sksMatkul4 + $sksMatkul5;
             $IPK = $totalBobot / $totalSKS; // ini ngitung IPK dengan cara total bobot dibagi total SKS
 
-            function display($nama, $sks, $hadir, $tugas, $uts, $uas, $akhir, $grade, $mutu, $bobot, $status) 
-            {// function ini buat nampilin data per matkul jadi ga usah nulis manual satu per satu yey :>
+            function display($nama, $sks, $hadir, $tugas, $uts, $uas, $akhir, $grade, $mutu, $bobot, $status)
+            { // function ini buat nampilin data per matkul jadi ga usah nulis manual satu per satu yey :>
                 echo "<h3>$nama</h3>";
                 echo "<p><strong>Credits:</strong> $sks</p>";
                 echo "<p><strong>Attandance:</strong> $hadir</p>";
@@ -188,14 +191,14 @@
                 echo "<p><strong>Status:</strong> $status</p>";
                 echo "<br>"; // echo ini buat display atau panggil output atau hasilnya, <br> sebagai penjarak tiap tiap matkul
             } // ntr akan otomatis ngegenerate semua data matkul sampai semua value yang udah distore habis teroutput 
-// biar ga usah nulis manual satu per satu struktur dalemnya ya kali :v
+            // biar ga usah nulis manual satu per satu struktur dalemnya ya kali :v
             display($namaMatkul1, $sksMatkul1, $nilaiHadir1, $nilaiTugas1, $nilaiUTS1, $nilaiUAS1, $nilaiAkhir1, $grade1, $mutu1, $bobot1, $status1);
             display($namaMatkul2, $sksMatkul2, $nilaiHadir2, $nilaiTugas2, $nilaiUTS2, $nilaiUAS2, $nilaiAkhir2, $grade2, $mutu2, $bobot2, $status2);
             display($namaMatkul3, $sksMatkul3, $nilaiHadir3, $nilaiTugas3, $nilaiUTS3, $nilaiUAS3, $nilaiAkhir3, $grade3, $mutu3, $bobot3, $status3);
             display($namaMatkul4, $sksMatkul4, $nilaiHadir4, $nilaiTugas4, $nilaiUTS4, $nilaiUAS4, $nilaiAkhir4, $grade4, $mutu4, $bobot4, $status4);
             display($namaMatkul5, $sksMatkul5, $nilaiHadir5, $nilaiTugas5, $nilaiUTS5, $nilaiUAS5, $nilaiAkhir5, $grade5, $mutu5, $bobot5, $status5);
-// nah display ini sebagai function yang ngegenerate semua data matkul yang udah disimpen didalam variabel-variabel sementara diatas jadi nama, sks, hadir, tugas, uts, uas, akhir, grade, mutu, bobot, status.
-// praktis singkat ga repot ga tebel jadi size filenya :v
+            // nah display ini sebagai function yang ngegenerate semua data matkul yang udah disimpen didalam variabel-variabel sementara diatas jadi nama, sks, hadir, tugas, uts, uas, akhir, grade, mutu, bobot, status.
+            // praktis singkat ga repot ga tebel jadi size filenya :v
             echo "<h3>Total Weight: " . number_format($totalBobot, 2) . "</h3>";
             echo "<h3>Total Credits: $totalSKS</h3>";
             echo "<br><h2>Cumulative GPA: " . number_format($IPK, 2) . "</h2>"; // nah ini buat pentotalannya pake number_format biar format desimalnya rapi dan terkostumisasi
