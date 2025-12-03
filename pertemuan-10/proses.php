adceede201d8c4af5547a42143d25a32a209e3c5
25111500007-Jeffre6098 <2511500007@mahasiswa.atmaluhur.ac.id>
2025-12-03 12:37:40 +0700

    copy paste file praktek pertemuan-11 ke pertemuan-10

---
+++ pertemuan-10/proses.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+$arrcontactus = [
+    "name" => $_POST["txtNama"] ?? "",
+    "email" => $_POST["txtEmail"] ?? "",
+    "pesan" => $_POST["txtPesan"] ?? ""
+];
+$_SESSION["contactus"] = $arrcontactus; 
+header("location: index.php#contact"); 
+
+$arrBiodata = [ 
+"nim" => $_POST["txtNim"] ?? "", 
+"nama" => $_POST["txtNmLengkap"] ?? "", 
+"tempat" => $_POST["txtT4Lhr"] ?? "", 
+"tanggal" => $_POST["txtTglLhr"] ?? "", 
+"hobi" => $_POST["txtHobi"] ?? "", 
+"pasangan" => $_POST["txtPasangan"] ?? "",
+"pekerjaan" => $_POST["txtKerja"] ?? "",
+"ortu" => $_POST["txtNmOrtu"] ?? "", 
+"kakak" => $_POST["txtNmKakak"] ?? "", 
+"adik" => $_POST["txtNmAdik"] ?? "" 
+]; 
+
+$_SESSION["biodata"] = $arrBiodata; 
+header("location: index.php#contact"); 
+
+?>
