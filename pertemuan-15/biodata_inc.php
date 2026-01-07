458fc0f9887d089be3f2238b1ca8435797de38a5
25111500007-Jeffre6098 <2511500007@mahasiswa.atmaluhur.ac.id>
2026-01-07 11:40:03 +0700

    Menyesuaikan isi kode file biodata_inc.php

--- pertemuan-15/biodata_inc.php
+++ pertemuan-15/biodata_inc.php
@@ -4,13 +4,20 @@
 require 'koneksi.php';
 require_once 'fungsi.php';
 
-$configField = [
-    "name" => ["label" => "Nama:", "suffix" => ""],
-    "email" => ["label" => "Email:", "suffix" => ""],
-    "pesan" => ["label" => "Pesan Anda:", "suffix" => ""]
+$fieldConfig = [
+  "nim" => ["label" => "NIM:", "suffix" => ""],
+  "nmlengkap" => ["label" => "Nama Lengkap:", "suffix" => " &#128526;"],
+  "tempatlhr" => ["label" => "Tempat Lahir:", "suffix" => ""],
+  "tanggal" => ["label" => "Tanggal Lahir:", "suffix" => ""],
+  "hobi" => ["label" => "Hobi:", "suffix" => " &#127926"],
+  "pasangan" => ["label" => "Pasangan:", "suffix" => " &hearts;"],
+  "pekerjaan" => ["label" => "Pekerjaan:", "suffix" => " &copy; 2025"],
+  "ortu" => ["label" => "Nama Orang Tua:", "suffix" => ""],
+  "kakak" => ["label" => "Nama Kakak:", "suffix" => ""],
+  "adik" => ["label" => "Nama Adik:", "suffix" => ""]
 ];
 
-$sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
+$sql = "SELECT * FROM tbl_biomhs ORDER BY bid DESC";
 $q = mysqli_query($conn, $sql);
 
 if (!$q) {
@@ -19,13 +26,19 @@ if (!$q) {
     echo "<p>Belum ada data tamu yang tersimpan.</p>";
 } else {
     while ($row = mysqli_fetch_assoc($q)) {
-    $arrcontactus = [
-        "name" => $row["cnama"] ?? "",
-        "email" => $row["cemail"] ?? "",
-        "pesan" => $row["cpesan"] ?? "",
-        "no" => $row["NO"] ?? "",
+    $arrBiodata = [
+        "nim" => $row ['bnim'] ?? "",
+        "nmlengkap" => $row["bnmlengkap"] ?? "",
+        "tempatlhr" => $row["btmptlhr"] ?? "",
+        "tanggal" => $row["btgllhr"] ?? "",
+        "hobi" => $row["bhobi"] ?? "",
+        "pasangan" => $row["bpasangan"] ?? "",
+        "pekerjaan" => $row["bpekerjaan"] ?? "",
+        "ortu" => $row["bnmortu"] ?? "",
+        "kakak" => $row["bnmkakak"] ?? "",
+        "adik" => $row["bnmadik"] ?? "",
     ];
-    echo tampilkanContactus($configField, $arrcontactus);
+    echo tampilkanContactus($fieldConfig, $arrBiodata);
 }
 }
 ?>
