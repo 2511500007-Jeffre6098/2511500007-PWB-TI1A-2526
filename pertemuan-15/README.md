8e3c9756f196257c385a3979a637383ef31aa2aa
25111500007-Jeffre6098 <2511500007@mahasiswa.atmaluhur.ac.id>
2026-01-07 11:16:33 +0700

    Membuat tabel baru tbl_biomhs pada database db_pwd2025

--- pertemuan-15/README.md
+++ pertemuan-15/README.md
@@ -7,3 +7,4 @@ Hari ini Kamis, 7 Januari 2026, saya mempelajari:
 <ol>
 <li>Copy Paste File Pertemuan 13 ke Folder Pertemuan 15</li>
 <li>Menduplikat file read.php dan rename menjadi biodata_read.php</li>
+<li>Membuat tabel baru "tbl_biomhs" pada database db_pwd2025</li>
--- pertemuan-15/koneksi.php
+++ pertemuan-15/koneksi.php
@@ -4,7 +4,7 @@ $user = "root";
 $pass = ""; 
 $db = "db_pwd2025";
 
-$conn = mysqli_connect(hostname: $host, username: $user, password: $pass, database: $db);
+$conn = mysqli_connect( $host,  $user,  $pass,  $db);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
