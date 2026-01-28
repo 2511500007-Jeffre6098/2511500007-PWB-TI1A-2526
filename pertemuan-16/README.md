b005e0885ffc61ead797595c19df10cdbc62e30f
25111500007-Jeffre6098 <2511500007@mahasiswa.atmaluhur.ac.id>
2026-01-28 10:23:41 +0700

    Membuat tabel baru iodata_dosen di database db_pwd2025

--- pertemuan-16/README.md
+++ pertemuan-16/README.md
@@ -1,1 +1,8 @@
 # pertemuan-16
+
+NIM: 2511500007
+Nama: Jeffrey Deryata
+
+Hari ini Rabu, 28 Januari 2026, saya mempelajari:
+<ol>
+<li>Membuat tabel baru `biodata_dosen` di database db_pwd2025</li>
---
+++ pertemuan-16/read_biodosen.php
@@ -0,0 +1,59 @@
+<?php
+  session_start();
+  require 'koneksi.php';
+  require 'fungsi.php';
+
+  $sql = "SELECT * FROM biodata_dosen ORDER BY kd_dosen DESC";
+  $q = mysqli_query($conn, $sql);
+  if (!$q) {
+    die("Query error: " . mysqli_error($conn));
+  }
+?>
+
+<?php
+  $flash_berhasil = $_SESSION['flash_berhasil'] ?? ''; #jika query sukses
+  $flash_gagal  = $_SESSION['flash_gagal'] ?? ''; #jika ada error
+  #bersihkan session ini
+  unset($_SESSION['flash_berhasil'], $_SESSION['flash_gagal']); 
+?>
+
+<?php if (!empty($flash_berhasil)): ?>
+        <div style="padding:10px; margin-bottom:10px; 
+          background:#d4edda; color:#155724; border-radius:6px;">
+          <?= $flash_berhasil; ?>
+        </div>
+<?php endif; ?>
+
+<?php if (!empty($flash_gagal)): ?>
+        <div style="padding:10px; margin-bottom:10px; 
+          background:#f8d7da; color:#721c24; border-radius:6px;">
+          <?= $flash_gagal; ?>
+        </div>
+<?php endif; ?>
+
+<table border="1" cellpadding="8" cellspacing="0">
+  <tr>
+    <th>No</th>
+    <th>Aksi</th>
+    <th>KD Dosen</th>
+    <th>Nama</th>
+    <th>Alamat</th>
+    <th>Tgl Mengajar</th>
+    <th>Created At</th>
+  </tr>
+  <?php $i = 1; ?>
+  <?php while ($row = mysqli_fetch_assoc($q)): ?>
+    <tr>
+      <td><?= $i++ ?></td>
+      <td>
+        <a href="edit.php?cid=<?= (int)$row['cid']; ?>">Edit</a>
+        <a onclick="return confirm('Hapus <?= htmlspecialchars($row['cnama']); ?>?')" href="proses_delete.php?cid=<?= (int)$row['cid']; ?>">Delete</a>
+      </td>
+      <td><?= $row['kd_dosen']; ?></td>
+      <td><?= htmlspecialchars($row['nm_dosen']); ?></td>
+      <td><?= htmlspecialchars($row['almt']); ?></td>
+      <td><?= htmlspecialchars($row['tgl_dosen']); ?></td>
+      
+    </tr>
+  <?php endwhile; ?>
+</table>
