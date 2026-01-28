b1c103b52027dbcab1908442601751ca695c49db
25111500007-Jeffre6098 <2511500007@mahasiswa.atmaluhur.ac.id>
2026-01-28 11:43:43 +0700

    Menambahkan isset value pada index.php dan Waktu tidak cukup pak :'>

---
+++ pertemuan-16/proses_deletebiodosen.php
@@ -0,0 +1,43 @@
+<?php
+  session_start();
+  require __DIR__ . '/koneksi.php';
+  require_once __DIR__ . '/fungsi.php';
+
+  #validasi cid wajib angka dan > 0
+  $cid = filter_input(INPUT_GET, 'kd_dosen', FILTER_VALIDATE_INT, [
+    'options' => ['min_range' => 1]
+  ]);
+
+  if (!$cid) {
+    $_SESSION['flash_gagal'] = 'kd_dosen Tidak Valid.';
+    redirect_ke('read_biodosen.php');
+  }
+
+  /*
+    Prepared statement untuk anti SQL injection.
+    menyiapkan query UPDATE dengan prepared statement 
+    (WAJIB WHERE cid = ?)
+  */
+  $stmt = mysqli_prepare($conn, "DELETE FROM biodata_dosen
+                                WHERE kd_dosen = ?");
+  if (!$stmt) {
+    #jika gagal prepare, kirim pesan error (tanpa detail sensitif)
+    $_SESSION['flash_gagal'] = 'Terjadi kesalahan sistem (prepare gagal).';
+    redirect_ke('read_biodosen.php');
+  }
+
+  #bind parameter dan eksekusi (s = string, i = integer)
+  mysqli_stmt_bind_param($stmt, "i", $kodedos);
+
+  if (mysqli_stmt_execute($stmt)) { #jika berhasil, kosongkan old value
+    /*
+      Redirect balik ke read.php dan tampilkan info sukses.
+    */
+    $_SESSION['flash_berhasil'] = 'Terima kasih, data Anda sudah dihapus.';
+  } else { #jika gagal, simpan kembali old value dan tampilkan error umum
+    $_SESSION['flash_gagal'] = 'Data gagal dihapus. Silakan coba lagi.';
+  }
+  #tutup statement
+  mysqli_stmt_close($stmt);
+
+  redirect_ke('read_biodosen.php');
